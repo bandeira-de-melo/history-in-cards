6b53416e541c9e82283231d9486ed6a47834a528
Thiago <trabalho.bandeira@gmail.com>
2023-04-06 12:22:14 -0300

    feat: momentServices

---
+++ src/services/momentServices.ts
@@ -0,0 +1,19 @@
+async function insertNewMoment() {
+  
+}
+
+async function getAllMoments() {
+  
+}
+
+async function getMomentById() {
+  
+}
+
+async function updateMoment() {
+  
+}
+
+async function deleteMoment() {
+  
+}
