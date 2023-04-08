7bfbeab288b92a7e004fdbc7502c8e6cb5bc42bd
Thiago <trabalho.bandeira@gmail.com>
2023-04-08 02:52:47 -0300

    feat: momentsRepository

--- src/repositories/momentRepository/momentsRepository.ts
+++ src/repositories/momentRepository/momentsRepository.ts
@@ -1,7 +1,24 @@
-async function create() {
-  
+import conflictError from "../../errors/conflictError.js"
+import { Moment } from "../../protocols.js"
+
+async function getMomentByTitle(momentTitle: string){
+  try {
+    const result = await asdf
+    return result
+  } catch (error) {
+    return error.message
+  }
+}
+async function create(moment: Moment) {
+  try {
+    return await
+  } catch (error) {
+    return error.message
+  }  
 }
 
+
 export default {
+  getMomentByTitle,
   create
 }
