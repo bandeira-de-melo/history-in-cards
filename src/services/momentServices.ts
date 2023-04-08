2627b0a76d725e482400bec6bae030e87a5b1782
Thiago <trabalho.bandeira@gmail.com>
2023-04-08 00:17:35 -0300

    feat: moments services file

--- src/services/momentServices.ts
+++ src/services/momentServices.ts
@@ -1,5 +1,11 @@
-async function insertNewMoment() {
-   
+import {Request, Response} from 'express';
+
+import momentsRepository from "../repositories/momentRepository/momentsRepository.js";
+import conflictError from '../errors/conflictError.js';
+
+async function insertNewMoment(req: Request, res: Response) {
+   const userName = await momentsRepository.getMomentByName();
+   if(userName) throw conflictError
 }
 
 async function getAllMoments() {
