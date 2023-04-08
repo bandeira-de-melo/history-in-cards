6c3e4fed2bfe43def1e9b5606203f9f6124e5a7c
Thiago <trabalho.bandeira@gmail.com>
2023-04-08 02:53:44 -0300

    refactor: add validateSchema middleware

--- src/routes/moments.routes.ts
+++ src/routes/moments.routes.ts
@@ -1,8 +1,9 @@
 import {Router} from 'express';
+import validateSchema from '../middlewares/validateSchema.js';
 
 const momentsRouter = Router();
 
-momentsRouter.post("/create")
+momentsRouter.post("/create", validateSchema())
 momentsRouter.get("/getall")
 momentsRouter.delete("/delete/:id")
 momentsRouter.put("/update/:id")
