0d70de4d14cb5984a9c855177baf2469fe9f7028
Thiago <trabalho.bandeira@gmail.com>
2023-04-08 02:50:51 -0300

    feat: momentsController

--- src/controllers/momentsController.ts
+++ src/controllers/momentsController.ts
@@ -1,7 +1,12 @@
 import {Request, Response} from 'express';
+import { Moment } from '../protocols.js';
+import httpStatus from 'http-status';
+import momentServices from '../services/momentServices.js';
 
-async function insertMoment(req: Request, res: Response){
-  
+async function createMoment(req: Request, res: Response){
+  const moment = res.locals.data as Moment;
+  await momentServices.insertMomentService(moment)
+  return res.status(httpStatus.CREATED).send("Moment Created") 
 }
 
 async function getMoments(req: Request, res: Response){
